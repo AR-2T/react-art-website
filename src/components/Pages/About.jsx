ac83f862758579749a131acafbf927ac39137310
Theo <justrandomess@gmail.com>
2022-10-08 17:21:45 -0700

    Messing Around With Curves

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import './about.css'
+
+
 
 function About() {
   return (
     <>
 
+    {/* <div className="space-y-100 aboutPage h-screen">
+      <div className ="section red text-7xl font-bold strokeAboutText">
+        ABOUT US
+
+        <div class="custom-shape-divider-bottom-1665272885">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+          </svg>
+        </div>
+
+      </div>
+    </div> */}
+
+    <div className="space-y-100 aboutPage h-screen">
+
+      <div class="spacer layer1">
+        ABOUT US
+      </div>
+      
+    </div>
+
+
+    {/*
     <div className="space-y-100">
 
-      {/* Mission Statement */}
       <dgroup>
         <div className="text-4xl font-bold font-sans underline text-black-400 py-4">
           MISSION STATEMENT
@@ -16,31 +41,26 @@ function About() {
         </div>
       </dgroup>
 
-      {/* About Us */}
       <dgroup>
         <div className="text-4xl font-bold font-sans underline text-black-400 py-4">
           OUR TEAM
         </div>
         <div className="grid grid-cols-4 gap-16 font-sans">
           
-          {/* Aaron's Information */}
           <dgroup>
             <div className = "text-2xl font-bold">Aaron Do</div>
           </dgroup>
 
-          {/* Rob's Information */}
           <dgroup>
             <div className = "text-2xl font-bold">Robert Balatbat</div>
             <div className = "font-italic">Robert enjoys playing League of Legends with his 5-stack.</div>
           </dgroup>
 
-          {/* Theo's Information */}
           <dgroup>
             <div className = "text-2xl font-bold">Theodore Tran</div>
             <div className = "font-italic">Theodore enjoys drinking copious amounts of boba while trying to grind out of being hardstuck Platinum in Valorant.</div>
           </dgroup>
 
-          {/* Ryan's Information */}
           <dgroup>
             <div className = "text-2xl font-bold">Ryan Yan</div>
             <div className = "font-italic"></div>
@@ -50,6 +70,7 @@ function About() {
       </dgroup>
       
     </div>
+    */}
 
     </>
   )
