196020ea7ece0abd09b106e9d1bfbb37268df339
Theo <justrandomess@gmail.com>
2022-10-17 22:07:40 -0700

    Ryan's Image Changed

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -83,7 +83,7 @@ function About() {
         </div>
 
         <div class="teams">
-          <img src="https://cdn.discordapp.com/attachments/818960742551519252/1029542003496718358/ryan_mushroom.png" height="50px " alt="" />
+          <img src="https://kirby.nintendo.com/assets/img/intro/kirby-star2.png" height="50px " alt="" />
           <div class="name">Ryan Yan</div>
           <div class="desig">Developer</div>
           <div class="about">[Enter personal information here]</div>
