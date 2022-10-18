16d82b83c6eeb42a5ffef3f46492e652dc322fb9
Melancholy22 <56004966+Melancholy22@users.noreply.github.com>
2022-10-17 21:51:59 -0700

    Updated User Profile

---
+++ src/components/Pages/CreatePost.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CreatePosts} from "../../ui-components"
+
+function CreatePost() {
+  return (
+    <div><CreatePosts /></div>
+  )
+}
+
+export default CreatePost
